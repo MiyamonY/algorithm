f8de45a2deea494aca53649fefb66341848ad1b6
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2017-06-28 12:37:00 +0900

    [add][aoj/alds2] bubble sortを追加

--- aoj/alds2/problems.hpp
+++ aoj/alds2/problems.hpp
@@ -13,3 +13,4 @@ using namespace std;
 
 extern int maximum_profit(vector<int32_t> &vec);
 extern void insersion_sort(int32_t array[], size_t size, void (*callback)(int32_t arrary[], size_t size));
+extern void bubble_sort(int32_t array[], size_t size, void (*callback)(int32_t arrary[], size_t size));

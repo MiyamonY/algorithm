e2266ae274c1158c34e0aeb61eaad40ab06036e5
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2017-06-28 11:57:19 +0900

    [add][aoj/alds2] insersion sortを追加

--- aoj/alds2/problems.hpp
+++ aoj/alds2/problems.hpp
@@ -12,3 +12,4 @@
 using namespace std;
 
 extern int maximum_profit(vector<int32_t> &vec);
+extern void insersion_sort(int32_t array[], size_t size, void (*callback)(int32_t arrary[], size_t size));

8f42d0e9fc192de6ad3e02ef485ef560a5e680c1
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2017-07-03 22:29:04 +0900

    [add][aoj/alds2] allocationを追加

--- aoj/alds2/test/test_allocation.cpp
+++ aoj/alds2/test/test_allocation.cpp
@@ -24,11 +24,16 @@ Test(Allocation, LoadCapacity)
   cr_assert_eq(allocation_load_capcity(loads, 8, 4), 5);
   cr_assert_eq(allocation_load_capcity(loads, 2, 3), 0);
   cr_assert_eq(allocation_load_capcity(loads, 12, 4), 7);
+  cr_assert_eq(allocation_load_capcity(loads, 9, 4), 6);
 }
 
 Test(Allocation, CalcMaxLoadCapacity)
 {
   vector<uint32_t> loads{3, 4, 5, 7, 8, 1, 2};
 
-  cr_assert_eq(allocation_max_load_capcity(loads, 3), );
+  cr_assert_eq(allocation_max_load_capcity(loads, 6), 8);
+  cr_assert_eq(allocation_max_load_capcity(loads, 5), 8);
+  cr_assert_eq(allocation_max_load_capcity(loads, 4), 11);
+  cr_assert_eq(allocation_max_load_capcity(loads, 2), 18);
+  cr_assert_eq(allocation_max_load_capcity(loads, 1), 30);
 }

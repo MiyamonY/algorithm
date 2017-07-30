bf652c2300a7f81ba9e32185752c752d2c2869c0
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2017-07-30 15:47:19 +0900

    [atcoder][abc/068] complete

---
+++ atcoder/abc/068/a.cpp
@@ -0,0 +1,20 @@
+///
+// File:  a.cpp
+// Author: ymiyamoto
+//
+// Created on Sat Jul 29 21:07:10 2017
+//
+
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
+
+int32_t main()
+{
+  uint32_t val;
+  cin >> val;
+
+  cout << "ABC" << val << endl;
+  return 0;
+}

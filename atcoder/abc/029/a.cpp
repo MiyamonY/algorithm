1047b5e886969dc6f91c9fc6e19703c200bf4e21
MIYAMOTO Yohei <miyamoto.youhei@jp.panasonic.com>
2018-01-11 11:31:27 +0900

    [add][atcoder/abc/029] complete a

---
+++ atcoder/abc/029/a.cpp
@@ -0,0 +1,18 @@
+///
+//  Author : Author
+// Created On : Thu Jan 11 2018
+//  File : a.cpp
+//
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int32_t main() {
+  string W;
+  cin >> W;
+
+  cout << W << "s" << endl;
+  return 0;
+}

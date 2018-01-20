d3599d4013ac3495e7143aff04e77b84b257603e
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-20 21:47:17 +0900

    [add][atcoder/abc/012] complete b

---
+++ atcoder/abc/012/b.cpp
@@ -0,0 +1,26 @@
+///
+// File:  b.cpp
+// Author: ymiyamoto
+//
+// Created on Sat Jan 20 21:41:11 2018
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(_n, _init, _N) for (int32_t _n = _init; _n < (int32_t)(_N); _n++)
+#define rrep(_n, _N, _end) for (int32_t _n = (int32_t)(_N); _n >= _end; _n--)
+#define iceil(_x, _y) (((_x) + (_y)-1) / (_y))
+#define ifloor(_x, _y) ((_x) / (_y))
+
+static const double pi = 2 * asin(1);
+static const double precision = 10e-9;
+
+int32_t main()
+{
+  uint32_t N;
+  cin >> N;
+
+  printf("%02d:%02d:%02d\n", N / 3600, (N % 3600) / 60, N % 60);
+
+  return 0;
+}

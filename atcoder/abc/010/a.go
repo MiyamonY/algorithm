4d0041426860967d6a85dad73d43d42015caf1cb
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-21 13:03:38 +0900

    [add][atcoder/abc/010] complete a

---
+++ atcoder/abc/010/a.go
@@ -0,0 +1,16 @@
+///
+// File:  a.go
+// Author: ymiyamoto
+//
+// Created on Sun Jan 21 13:02:13 2018
+//
+package main
+
+import "fmt"
+
+func main(){
+	var S string
+	fmt.Scan(&S)
+
+	fmt.Println(S+"pp")
+}

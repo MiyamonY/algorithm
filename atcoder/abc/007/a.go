b6993afd7c2d749ae5d033268786372f04be9ee9
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-21 15:23:17 +0900

    [add][atcoder/abc/007] complete a

---
+++ atcoder/abc/007/a.go
@@ -0,0 +1,16 @@
+///
+// File:  a.go
+// Author: ymiyamoto
+//
+// Created on Sun Jan 21 15:14:30 2018
+//
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	fmt.Println(n - 1)
+}

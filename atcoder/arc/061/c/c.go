1e4abc9cbd4bffa11f10c1ca37b2b0e091955cfe
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-03-01 21:54:41 +0900

    [add][atcoder/arc/061] complete c

---
+++ atcoder/arc/061/c/c.go
@@ -0,0 +1,35 @@
+///
+// File:  c.go
+// Author: ymiyamoto
+//
+// Created on Thu Mar  1 21:37:20 2018
+//
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var S string
+	fmt.Scan(&S)
+
+	count := 0
+	for i := 0; i < int(1<<uint32(len(S))); i++ {
+		tmp := i
+		cur := tmp & 1
+		start := 0
+		for j := 0; j < len(S); j++ {
+			if cur != tmp&1 {
+				val, _ := strconv.ParseInt(S[start:j], 10, 64)
+				count += int(val)
+				start = j
+			}
+			tmp >>= 1
+		}
+		val, _ := strconv.ParseInt(S[start:len(S)], 10, 64)
+		count += int(val)
+	}
+	fmt.Println(count / 2)
+}

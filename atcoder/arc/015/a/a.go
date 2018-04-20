360cf40894cb9fae71dc21917ac9e12a5ec835b6
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-04-20 22:01:55 +0900

    [add][atcoder/arc/015] complete a

---
+++ atcoder/arc/015/a/a.go
@@ -0,0 +1,18 @@
+///
+// File:  a.go
+// Author: ymiyamoto
+//
+// Created on Fri Apr 20 22:00:44 2018
+//
+package main
+
+import (
+	"fmt"
+)
+
+var n float64
+
+func main() {
+	fmt.Scan(&n)
+	fmt.Println(9*n/5 + 32)
+}

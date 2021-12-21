67e7857fcf339774a60c0306c08fa159700346f8
MIYAMOTO Yohei <miyamoto@heartorg.co.jp>
2021-12-21 12:27:41 +0900

    [Add] index BinarySearch

--- BinarySearch/ocaml/main.ml
+++ BinarySearch/ocaml/main.ml
@@ -1,13 +1,22 @@
 open StdLabels
 
-let binary_search arr ~cmp =
-  let rec aux l h =
-    let m = (l + h) / 2 in
-    if l + 1 = h then h
-    else if cmp arr.(m) then aux m h
-    else aux l m
-  in
-  aux (-1) @@ Array.length arr
+module BinarySearch = struct
+  let search n ~f =
+    let rec aux l h =
+      let m = (l + h) / 2 in
+      if h - l = 1 then h
+      else if f m then aux l m
+      else aux m h
+    in aux (-1) n
+
+  let array arr ~f =
+    let rec aux l h =
+      let m = (l + h) / 2 in
+      if l + 1 = h then h
+      else if f arr.(m) then aux m h
+      else aux l m
+    in aux (-1) @@ Array.length arr
+end
 
 let () =
   let (n, q) = Scanf.scanf "%d %d" (fun a b -> (a, b)) in
@@ -19,5 +28,5 @@ let () =
   Array.sort ~cmp:compare arr;
   List.iter (List.init q (fun _ -> 0)) ~f:(fun _ ->
       let x = Scanf.scanf "%d" (fun x -> x) in
-      let i = binary_search arr ~cmp:(fun v -> v < x) in
+      let i = BinarySearch.array arr ~f:(fun v -> v < x) in
       Printf.printf "%d\n" @@ n - i)
